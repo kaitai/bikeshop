bc19855e4f2f44a157eeb0f43718b3bb64fc2156
kaitai <kaisa.taipale@gmail.com>
2014-04-07 18:22:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bikeshop::Application.config.secret_key_base = 'a1a804d82c15534f9d7c44fdbbb21f7745432eb83c7b77fd3f926fb2ec88ed475fe45b2a37dd6385de5c3a7193d35332ec875666b17473b112c63167cfbd7f85'
